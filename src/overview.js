d85cebcf29f3d662189dafb62a6b88c4f1baf9bc
安慶名　泰征 <t-agena@maki.ne.jp>
2023-07-11 14:50:53 +0900

    overview use my api

--- src/overview.js
+++ src/overview.js
@@ -13,20 +13,12 @@ export default function OverView({ movieId, setMovieId }) {
     backgroundSize: "cover",
   };
 
-  const movieOverview = {
-    method: "GET",
-    url: "https://api.themoviedb.org/3/movie/" + movieId,
-    params: { language: "ja-JP" },
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjIyNzRiMzJjMTgwM2ZmNWJmMGFkYjg2ZmNiZmQ4ZCIsInN1YiI6IjY0NmRjYjc0OTY2MWZjMDExZDk1NzlhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6abkUx93_XOwAaIbBxBseBK-1KBWYpoBLMKoIdQ7U9I",
-    },
-  };
-
   const getOverview = async () => {
     try {
-      const overviewResults = await axios.request(movieOverview);
+      const overviewResults = await axios.request({
+        method: "GET",
+        url: process.env.REACT_APP_API_URL + "/detail?query=" + movieId,
+      });
       if (overviewResults.data) {
         console.log(overviewResults.data);
         setViewData(overviewResults.data);
